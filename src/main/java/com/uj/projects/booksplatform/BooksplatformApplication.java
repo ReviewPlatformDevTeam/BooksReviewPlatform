7c6497e1ea6195de402b9c1f3feb88c474049db3
Krystian <krystiane1@onet.pl>
2020-03-16 14:34:19 +0100

    Project init structure

---
+++ src/main/java/com/uj/projects/booksplatform/BooksplatformApplication.java
@@ -0,0 +1,13 @@
+package com.uj.projects.booksplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksplatformApplication.class, args);
+	}
+
+}
