fbe187894338bc1ffa78e53c8f7b51558d2f3b65
SzymonSmykala <ctk7000@gmail.com>
2020-03-29 15:16:03 +0200

    cleaned up

--- server/src/main/java/com/uj/projects/booksplatform/user/service/LoginService.java
+++ server/src/main/java/com/uj/projects/booksplatform/user/service/LoginService.java
@@ -1,9 +1,7 @@
 package com.uj.projects.booksplatform.user.service;
 
 import com.uj.projects.booksplatform.user.entity.LoginResult;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface LoginService {
     public LoginResult Login(String username, String password);
 }
