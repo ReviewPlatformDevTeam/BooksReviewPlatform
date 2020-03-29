365283a9098aa945b6aaac0b3f24a7c359d16a74
SzymonSmykala <ctk7000@gmail.com>
2020-03-29 15:42:48 +0200

    Added jwt filter

---
+++ server/src/main/java/com/uj/projects/booksplatform/JwtFilter.java
@@ -0,0 +1,38 @@
+package com.uj.projects.booksplatform;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Base64;
+
+public class JwtFilter implements javax.servlet.Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        String header = httpServletRequest.getHeader("authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new ServletException("Missing or invalid Authorization header");
+        } else {
+            try {
+
+                String encoded = Base64.getEncoder().encodeToString("dupa123".getBytes());
+                String token = header.substring(7);
+                Claims claims = Jwts.parser().setSigningKey(encoded).parseClaimsJws(token).getBody();
+                servletRequest.setAttribute("claims", claims);
+            } catch (final SignatureException e) {
+                throw new ServletException("Invalid token");
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
