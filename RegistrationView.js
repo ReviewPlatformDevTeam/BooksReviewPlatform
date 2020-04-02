e0b1a4832bf748a72598ab906a0bae30f4e4dbeb
PaulGrishunin <50253805+PaulGrishunin@users.noreply.github.com>
2020-04-02 07:49:01 +0200

    Add files via upload

---
+++ RegistrationView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class RegistrationView extends Component {
+  render() {
+  return (
+    <div className="Registration" style={{height: "100vh"}}>
+        Sign up here!
+    </div>);
+  }
+
+}
+
+export default RegistrationView;
