809bf752e90c920223ab955a3807fff1ddbc10ca
wiktorks <wiktorks1994@gmail.com>
2020-04-08 23:23:47 +0200

    Login component and authorisation (#53)
    * Login component and authorisation
    
    * Added redirect function, removed comments
    
    * Created global object userKey
    
    * Fixed URL for login
    
    * Added /signedin route

--- client/src/Authorization/LoginView.js
+++ client/src/Authorization/LoginView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Login from './Login/Login';
 
 
 class LoginView extends Component {
   render() {
   return (
     <div className="Login" style={{height: "100vh"}}>
-        Login here!
+        <Login />
     </div>);
   }
 
