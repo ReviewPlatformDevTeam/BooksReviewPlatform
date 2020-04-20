32bd6421233881951492f12dc1e197acdca9b1e1
SzymonSmykala <ctk7000@gmail.com>
2020-04-20 18:37:49 +0200

    added BuildApp.sh, BuildApp.bat

---
+++ BuildApp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VERSION=$1
+docker build --no-cache -t booksplatform:0.0.$VERSION -f Dockerfile .
+docker run --rm -p 8080:8080 --name booksplatform-0.0.$VERSION booksplatform:0.0.$VERSION
