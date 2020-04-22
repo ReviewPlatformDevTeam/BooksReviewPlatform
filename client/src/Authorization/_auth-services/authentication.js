322fba378c47cdf6b9674513454ff15549a3d3c8
wiktorks <wiktorks1994@gmail.com>
2020-04-22 09:23:06 +0200

    Fixed HTTP PUST JSON body for /login (#87)
    * Login component and authorisation
    
    * Added redirect function, removed comments
    
    * Created global object userKey
    
    * Fixed URL for login
    
    * Added /signedin route
    
    * Fixed padding issue in Login.css
    
    * Fixed HTTP Body request JSON format in login
    
    * Fixed HTTP request body JSON

--- client/src/Authorization/_auth-services/authentication.js
+++ client/src/Authorization/_auth-services/authentication.js
@@ -9,15 +9,18 @@ const checkStatus = (response) => {
 
 const login = (username, password) => {
     const url = "/login";
+    const loginBody = {
+        username: username,
+        password: password
+    };
+
     const fetchData = {
         method: "POST",
-        body: {
-            username: username,
-            password: password
-        },
+        mode: 'cors',
         headers: {
             "Content-type": "application/json"
-        }
+        },
+        body: JSON.stringify(loginBody)
     };
 
     return fetch(url, fetchData)
