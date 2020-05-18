26a7e3ed31cb1d56177cb0e0eae64a93a2b44ed7
SzymonSmykala <ctk7000@gmail.com>
2020-05-18 09:01:37 +0200

    added methods in categoryController

--- server/src/main/java/com/uj/projects/booksplatform/category/controller/CategoryController.java
+++ server/src/main/java/com/uj/projects/booksplatform/category/controller/CategoryController.java
@@ -1,26 +1,30 @@
 package com.uj.projects.booksplatform.category.controller;
 
 import com.uj.projects.booksplatform.category.entity.Category;
-import com.uj.projects.booksplatform.category.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.uj.projects.booksplatform.category.service.CategoryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
-    private CategoryRepository categoryRepository;
+    private CategoryService categoryService;
 
-    @Autowired
-    public CategoryController(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @GetMapping
-    public void insertCategory(){
-        categoryRepository.save(new Category( "myName13"));
+    public List<Category> getAll(){
+        return categoryService.getAll();
+    }
+
+    @PostMapping
+    public Category create(@Valid @RequestBody Category category){
+        return categoryService.create(category);
     }
 }
