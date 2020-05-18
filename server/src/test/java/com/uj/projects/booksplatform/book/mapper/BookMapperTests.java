8521c53520d9641b78687ca96e0369be0742fb5e
SzymonSmykala <ctk7000@gmail.com>
2020-05-18 08:43:12 +0200

    added mappers tests

--- server/src/test/java/com/uj/projects/booksplatform/book/mapper/BookMapperTests.java
+++ server/src/test/java/com/uj/projects/booksplatform/book/mapper/BookMapperTests.java
@@ -4,16 +4,25 @@ import autofixture.publicinterface.Any;
 import com.uj.projects.booksplatform.book.dto.BookDto;
 import com.uj.projects.booksplatform.book.entity.Book;
 import com.uj.projects.booksplatform.category.entity.Category;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 import org.testng.Assert;
 
+import javax.validation.constraints.NotBlank;
+
 public class BookMapperTests {
 
+    private BookMapper sut;
+
+    @BeforeEach
+    public void setUp(){
+        sut = Mappers.getMapper( BookMapper.class );
+    }
+
     @Test
-    public void shouldMapCategory(){
+    public void shouldMapCategoryWhileMappingBookToBookDto(){
         // arrange
-        BookMapper sut = Mappers.getMapper( BookMapper.class );
         Book book = new Book();
         String categoryName = Any.string();
         Category category = new Category(categoryName);
@@ -21,8 +30,21 @@ public class BookMapperTests {
 
         // act
         BookDto actual = sut.bookToBookDto(book);
-
         // assert
         Assert.assertEquals(actual.getCategory(), categoryName);
+    }
+
+    @Test
+    public void shouldMapCategoryWhileMappingBookDtoToBook(){
+        // arrange
+        BookDto bookDto = new BookDto();
+        String categoryName = Any.string();
+        bookDto.setCategory(categoryName);
+        // act
+        Book actual = sut.bookDtoToBook(bookDto);
+
+        // assert
+        Assert.assertNotNull(actual.getCategory());
+        Assert.assertEquals(actual.getCategory().getName(), categoryName);
     }
 }
