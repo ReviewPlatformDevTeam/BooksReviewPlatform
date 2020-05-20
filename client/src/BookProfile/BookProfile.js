cc544f7b71cb3b5e7f218f30f82df2c73447f48a
Poreba <zanetaporeba@gmail.com>
2020-05-20 21:07:34 +0200

    add book profile reviews component

--- client/src/BookProfile/BookProfile.js
+++ client/src/BookProfile/BookProfile.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import bookMock from '../Mocks/books.json';
 import reviewsMock from '../Mocks/reviews.json';
-import { Card, Comment, List, Layout } from 'antd';
+import { Card, Comment, List, Layout, Form, Button, Input } from 'antd';
 import './BookProfile.css';
 import Rater from 'react-rater';
 import { withRouter } from "react-router-dom";
 import defaultBook from '../pictures/bookSmile.png';
+import moment from 'moment';
 
 const { Meta } = Card;
+const { TextArea } = Input;
 const defaultBookId = 1;
 
 export class BookProfile extends Component {
@@ -24,9 +26,67 @@ export class BookProfile extends Component {
         this.state = {
             bookId: bookId,
             bookData: undefined,
-            reviews: undefined
+            reviews: undefined,
+            submitting: false,
+            value: '',
+            score: 0
         }
     }
+
+    editor = () => (
+        <div>
+          <div style={{fontSize: 'x-large'}}>
+            <Rater total={5} rating={this.state.score} onRate={this.handleRate} interactive={true}/>
+          </div>
+          <Form.Item>
+            <TextArea rows={4} onChange={this.handleChange} value={this.state.value} />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" loading={this.state.submitting} onClick={this.handleSubmit} type="primary">
+              Add Review
+            </Button>
+          </Form.Item>
+        </div>
+      ); 
+
+    handleSubmit = () => {
+        if (!this.state.value) {
+          return;
+        }
+    
+        this.setState({
+          submitting: true,
+        });
+    
+        setTimeout(() => {
+            this.setState({
+                submitting: false,
+                value: '',
+                score: 0,
+                reviews: [
+                {
+                    author: 'Username',
+                    date: moment().format('YYYY-MM-DD'),
+                    review: this.state.value,
+                    score: this.state.score
+                },
+                ...this.state.reviews,
+                ],
+            });
+            }, 1000);
+      };
+
+    handleChange = e => {
+        this.setState({
+          value: e.target.value
+        });
+      }; 
+
+    handleRate = e => {
+        this.setState({
+            score: e.rating
+        });
+      };   
 
     componentDidMount() {
         this.loadBookData();
@@ -90,10 +150,10 @@ export class BookProfile extends Component {
                 <strong>Author: </strong>{this.state.bookData.author}<br />
                 <strong>Release date: </strong>{this.state.bookData.releaseDate}<br />
                 <br />
-                <p style={{fontSize: 'x-large'}}>
+                <div style={{fontSize: 'x-large'}}>
                     <Rater total={5} rating={this.state.bookData.score} interactive={false}/>
                     {this.state.bookData.numOfReviews} rated
-                </p>
+                </div>
             </Card>
         );
     }
@@ -102,8 +162,7 @@ export class BookProfile extends Component {
         return (
             <div>
                 <strong style={{paddingRight: '10px'}}>{item.author}</strong>
-                {item.date}
-                <p style={{fontSize: 'large'}}><Rater total={5} rating={item.score} interactive={false}/> {item.score}</p>
+                <div style={{fontSize: 'large'}}><Rater total={5} rating={item.score} interactive={false}/> {item.score}</div>
             </div>
         );
     }
@@ -115,23 +174,36 @@ export class BookProfile extends Component {
                 header={"Reviews"}
                 itemLayout="horizontal"
                 dataSource={this.state.reviews}
+                pagination={{
+                    pageSize: 4,
+                  }}
                 renderItem={item => (
-                <li>
+                <List.Item>
                     <Comment
                     author={this.commentHeader(item)}
+                    datetime={item.date}
                     avatar={'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'}
                     content={item.review}
                     />
-                </li>
+                </List.Item>
                 )}
             />
+        );
+    }
+
+    commentInput = () => {
+        return(
+                <Comment
+                content={
+                        <this.editor/>
+                    }/>
         );
     }
 
     render() {
         return (
             <div className="bookProfile" style={{height: "100vh"}}>
-                <Layout style={{paddingTop: 50, backgroundColor: 'white'}} className='bookLayout'>
+                <Layout style={{paddingTop: 50, backgroundColor: 'white'}} className='bookLayout'>   
                     {
                         this.state.bookData ?
                             <div>
@@ -143,6 +215,7 @@ export class BookProfile extends Component {
                             </div>: null
                     }
                 </Layout>
+                <div className='commentInput'>{this.commentInput()}</div>
                 <Layout style={{backgroundColor: 'white'}}>
                     { 
                         this.state.reviews  ? 
