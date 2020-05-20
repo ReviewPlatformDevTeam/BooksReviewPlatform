334c5e21a239c31d6afd30d143ee9d7ee0bcf8fe
Poreba <zanetaporeba@gmail.com>
2020-05-20 16:02:55 +0200

    add json mock, book review comment component

--- client/src/BookProfile/BookProfile.js
+++ client/src/BookProfile/BookProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import data from '../Mocks/books.json';
-import { Card } from 'antd';
+import bookMock from '../Mocks/books.json';
+import reviewsMock from '../Mocks/reviews.json';
+import { Card, Comment, List, Layout } from 'antd';
 import './BookProfile.css';
 import Rater from 'react-rater';
 import { withRouter } from "react-router-dom";
@@ -23,21 +24,27 @@ export class BookProfile extends Component {
         this.state = {
             bookId: bookId,
             bookData: undefined,
+            reviews: undefined
         }
     }
 
     componentDidMount() {
         this.loadBookData();
+        this.loadReviews();
     }
 
     loadBookData() {
-        let bookData = Object.keys(data).filter(item => { return data[item].id === this.state.bookId })[0];
-        bookData = data[bookData];
+        let bookData = Object.keys(bookMock).filter(item => { return bookMock[item].id === this.state.bookId })[0];
+        bookData = bookMock[bookData];
 
         if(bookData.image.length === 0) {
             bookData.image = defaultBook;
         }
         this.setState({bookData: bookData})
+    }
+
+    loadReviews() {
+        this.setState({reviews: reviewsMock.reviews});
     }
 
     createBookCard = () => {
@@ -91,20 +98,59 @@ export class BookProfile extends Component {
         );
     }
 
+    commentHeader = (item) => {
+        return (
+            <div>
+                <strong style={{paddingRight: '10px'}}>{item.author}</strong>
+                {item.date}
+                <p style={{fontSize: 'large'}}><Rater total={5} rating={item.score} interactive={false}/> {item.score}</p>
+            </div>
+        );
+    }
+
+    createReviewComponent = () => {
+        return(
+            <List
+                className="commentList"
+                header={"Reviews"}
+                itemLayout="horizontal"
+                dataSource={this.state.reviews}
+                renderItem={item => (
+                <li>
+                    <Comment
+                    author={this.commentHeader(item)}
+                    avatar={'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'}
+                    content={item.review}
+                    />
+                </li>
+                )}
+            />
+        );
+    }
+
     render() {
         return (
             <div className="bookProfile" style={{height: "100vh"}}>
-            {
-                this.state.bookData ?
-                    <div>
-                        {this.createBookCard()}
-                        <div className='bookInfo'>
-                            {this.createDescriptionCard()}
-                            {this.createDetailsCard()}
-                        </div>
-                    </div>
-                    : null
-            }
+                <Layout style={{paddingTop: 50, backgroundColor: 'white'}} className='bookLayout'>
+                    {
+                        this.state.bookData ?
+                            <div>
+                                {this.createBookCard()}
+                                <div className='bookInfo'>
+                                    {this.createDescriptionCard()}
+                                    {this.createDetailsCard()}
+                                </div>
+                            </div>: null
+                    }
+                </Layout>
+                <Layout style={{backgroundColor: 'white'}}>
+                    { 
+                        this.state.reviews  ? 
+                            <div className='commentSection'>
+                                {this.createReviewComponent()}
+                            </div>: null
+                    }
+                </Layout>
             </div>
         );
     }
