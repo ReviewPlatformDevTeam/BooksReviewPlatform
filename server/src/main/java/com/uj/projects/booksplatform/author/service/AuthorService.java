bd23c618a54d8c3e61b9dc68dd36f5d7400d524f
Szymon <ctk7000@gmail.com>
2020-05-21 21:11:12 +0200

    implemented repository

--- server/src/main/java/com/uj/projects/booksplatform/author/service/AuthorService.java
+++ server/src/main/java/com/uj/projects/booksplatform/author/service/AuthorService.java
@@ -1,14 +1,13 @@
 package com.uj.projects.booksplatform.author.service;
 
-
 import com.uj.projects.booksplatform.author.entity.Author;
 
 import java.util.List;
 
 public interface AuthorService {
     List<Author> getAll();
-    Author create(Author category);
+    Author create(Author author);
     void delete(Integer id);
-    Author update(Author category);
+    Author update(Author author);
     Author getById(Integer id);
 }
