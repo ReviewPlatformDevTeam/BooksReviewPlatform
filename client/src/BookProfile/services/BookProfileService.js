3fbedd2fc4bc9e4b3299406ae04a60faaf635023
Poreba <zanetaporeba@gmail.com>
2020-05-27 22:56:37 +0200

    sonar - changes for changes

--- client/src/BookProfile/services/BookProfileService.js
+++ client/src/BookProfile/services/BookProfileService.js
@@ -1,24 +1,26 @@
 import { commonService } from '../../Common/services/CommonService';
+
 const API_URL = process.env.REACT_APP_API_URL;
 
 const getBookById = (id) => {
     const url = `${API_URL}/books/${id}`;
 
-    let myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${commonService.getToken()}`);
-
-    let fetchData = {
+    let singleBookHeaders = new Headers();
+    let singleBookData = {
         method: 'GET',
-        mode: "cors",
         headers: myHeaders,
-        redirect: 'follow'
+        redirect: 'follow',
+        mode: "cors",
     };
 
-    return fetch(url, fetchData)
-        .then(response => commonService.checkStatus(response))
-        .then(response => response.text())
-        .then(response => JSON.parse(response))
-        .catch(error => {
+    singleBookHeaders.append(
+                            "Authorization", 
+                            `Bearer ${commonService.getToken()}`
+                            );
+
+    return fetch(url, singleBookData)
+        .then(response => commonService.checkStatus(response)).then(response => response.text())
+        .then(response => JSON.parse(response)).catch(error => {
             return {success: false, status: error.message}
         });
 }
