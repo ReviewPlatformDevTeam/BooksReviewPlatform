3b5f362ce93b985deb9a96bba6852eb05f40dfdf
kris875 <62203003+kris875@users.noreply.github.com>
2020-06-09 09:33:55 +0200

    Review crud + UserReviews + Search book/author (#139)
    * #Added Reviews CRUD
    #connected Review with Book
    #changed database schema to auto generated from entities
    #prepared start data migration
    
    * #Added searching review by User
    #Refactored
    
    * #db
    
    * #Added searching for book and author
    
    * #Added Tests
    
    * #Refactored after CR
    
    Co-authored-by: Krystian <krystiane1@onet.pl>

---
+++ server/src/main/java/com/uj/projects/booksplatform/review/repository/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.uj.projects.booksplatform.review.repository;
+
+import com.uj.projects.booksplatform.book.entity.Book;
+import com.uj.projects.booksplatform.review.entity.Review;
+import com.uj.projects.booksplatform.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findReviewsByBook(Book book);
+    List<Review> findReviewsByUser(User user);
+    Integer countByBook(Book book);
+}
